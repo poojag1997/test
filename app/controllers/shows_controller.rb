5403770c6da36a38eb7357c63b29e49c84801000
pooja <poojagupta.ujjian@gmail.com>
2020-11-20 20:58:21 +0530

    quick add

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -5,21 +5,23 @@ class ShowsController < ApplicationController
   end
 
   def new
-    @show = Show.new()
+    city = City.find_by(id: params[:city_id])
+    @show = city.shows.new()
   end
 
   def create
-    @show = Show.new(show_params)
+    city = City.find_by(id: params[:city_id])
+    @show = city.shows.new()
     @show.save
   end
 
   def edit
     @show = Show.find_by(id: params[:id])
-    @show.save
   end
 
   def update
-    
+    @show = Show.find_by(id: params[:id])
+    @show.update(show_params)
   end
 
   private 
